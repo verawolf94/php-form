f1b4de56b2af6b42aa8bbaa7a9f954c4ee51db21
Wolf <72465439+verawolf94@users.noreply.github.com>
2020-11-24 11:05:11 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 //echo '<p>This is PHP!</p>';
 
-if(isset($_GET["First Name"])){//show feedback
-  //echo $_GET["First Name"];
+if(isset($_GET["FirstName"])){//show feedback
+  //echo $_GET["FirstName"];
   
   echo '<pre>';
   var_dump($_GET);
@@ -11,7 +11,7 @@ if(isset($_GET["First Name"])){//show feedback
 }else{//show form
   echo '
   <form action ="">
-  First Name <input typr="text" name="First Name" />
+  First Name <input type="text" name="FirstName" />
   <br />
   <input type="submit">
   
